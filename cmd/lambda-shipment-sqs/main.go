6c0299bacddf2c50298a292ae9809839ed316399
retgits <lstigter@gmail.com>
2020-02-24 11:34:41 -0800

    feat: add sentry tracing

--- cmd/lambda-shipment-sqs/main.go
+++ cmd/lambda-shipment-sqs/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"fmt"
+	"os"
+	"time"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/retgits/acme-serverless-shipment"
+	"github.com/getsentry/sentry-go"
+	shipment "github.com/retgits/acme-serverless-shipment"
 	"github.com/retgits/acme-serverless-shipment/internal/emitter"
 	"github.com/retgits/acme-serverless-shipment/internal/emitter/sqs"
 	"github.com/retgits/acme-serverless-shipment/internal/shipper"
 )
 
 func handler(request events.SQSEvent) error {
+	sentrySyncTransport := sentry.NewHTTPSyncTransport()
+	sentrySyncTransport.Timeout = time.Second * 3
+
+	sentry.Init(sentry.ClientOptions{
+		Dsn:         os.Getenv("SENTRY_DSN"),
+		Transport:   sentrySyncTransport,
+		ServerName:  os.Getenv("FUNCTION_NAME"),
+		Release:     os.Getenv("VERSION"),
+		Environment: os.Getenv("STAGE"),
+	})
+
 	req, err := shipment.UnmarshalShipmentEvent([]byte(request.Records[0].Body))
 	if err != nil {
+		sentry.CaptureException(fmt.Errorf("error unmarshalling shipment event: %s", err.Error()))
 		return err
 	}
 
@@ -27,9 +44,21 @@ func handler(request events.SQSEvent) error {
 		Data: data,
 	}
 
+	sentry.AddBreadcrumb(&sentry.Breadcrumb{
+		Category:  "SentShipment",
+		Timestamp: time.Now().Unix(),
+		Level:     sentry.LevelInfo,
+		Data: map[string]interface{}{
+			"TrackingNumber": data.TrackingNumber,
+			"OrderNumber":    data.OrderNumber,
+			"Status":         data.Status,
+		},
+	})
+
 	em := sqs.New()
 	err = em.Send(evt)
 	if err != nil {
+		sentry.CaptureException(fmt.Errorf("error sending shipment event: %s", err.Error()))
 		return err
 	}
 
@@ -45,7 +74,26 @@ func handler(request events.SQSEvent) error {
 		Data: data,
 	}
 
-	return em.Send(evt)
+	sentry.AddBreadcrumb(&sentry.Breadcrumb{
+		Category:  "DeliveredShipment",
+		Timestamp: time.Now().Unix(),
+		Level:     sentry.LevelInfo,
+		Data: map[string]interface{}{
+			"TrackingNumber": data.TrackingNumber,
+			"OrderNumber":    data.OrderNumber,
+			"Status":         data.Status,
+		},
+	})
+
+	err = em.Send(evt)
+	if err != nil {
+		sentry.CaptureException(fmt.Errorf("error sending DeliveredShipment event: %s", err.Error()))
+		return err
+	}
+
+	sentry.CaptureMessage(fmt.Sprintf("order [%s] successfully delivered", req.Request.OrderID))
+
+	return nil
 }
 
 // The main method is executed by AWS Lambda and points to the handler
